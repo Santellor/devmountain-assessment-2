5015d6456252b0dd82b31a6c10e5f428faad44ae
Jacob Smith <2017JTS2019@gmail.com>
2024-05-05 17:01:52 -0600

    Completed the further study! It was hard, but I enjoyed it!

--- src/further-study.js
+++ src/further-study.js
@@ -31,6 +31,52 @@
 // Ex.:
 //   buildWordChain(['zoo', 'sour', 'racket', 'octos']);
 //   => ['zoo', 'octos', 'sour', 'racket']
-function buildWordChain(words) {}
 
+const buildWordChain = (words) => {
+
+    // empty arrays are not my friend, and neither is an empty argument
+    if (words === undefined || words[0] === undefined) {
+    return words
+    }
+
+    // Create an object that will store letter matches for each given element
+    const codex = {}
+    // { 
+    // zoo: [ 'octos' ], 
+    // sour: [ 'racket' ], 
+    // racket: [], 
+    // octos: [ 'sour' ] 
+    // }
+        
+    // Loop through words to fill codex
+    for (let word of words) {
+        //takes the last letter of each word
+        let finalLetter = word.slice(word.length - 1)
+        //populates the codex: key = word, value = array of words that start with finalLetter
+        //avoid any loop problems by preventing the word from being added to its own codex
+        codex[word] = words.filter((element) => element.startsWith(finalLetter) && element !== word)
+    }
+        
+    //declares the start of the chain as the first word in the given array
+    const chainArray = [words[0]]
+    
+    //loop through chain to place the correct next word from the codex
+    for (let word of chainArray) {
+
+        //defines the next word by searching the codex and returning the first value
+        nextWord = codex[word][0]
+
+        //checks if the next word is undefined, and pushes it if not
+        if (nextWord !== undefined) {
+        chainArray.push(nextWord)
+
+        //removes any items from the codex that are present in the chain
+        codex[nextWord] = codex[nextWord].filter((element) => !chainArray.includes(element))
+        delete codex[word]
+        }
+    }
+
+    return chainArray
+
+}
 export { buildWordChain };
